f5de927ea716903844fc5c9aff8209fda7751adb
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-07-14 18:57:00 +0800

    Added in various sliders

--- src/pageModules/coverPage.js
+++ src/pageModules/coverPage.js
@@ -1,6 +1,8 @@
 import React, { Component, useEffect, useRef, useState } from 'react'
 import '../App.css'
 import DatePicker from 'react-date-picker'
+import { Slider } from '@material-ui/core';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 
 export default function CoverPage(props) {
     return (
@@ -43,31 +45,75 @@ export default function CoverPage(props) {
 
     const [fromDate, fromDateChange] = useState(new Date())
     const [toDate, toDateChange] = useState(new Date())
+    const [startingFiat, startingFiatChange] = useState(0)
+    const [startingCrypto, startingCryptoChange] = useState(0)
+    const [tradingFee, tradingFeeChange] = React.useState(1);
+    const [coinPercent, coinPercentChange] = React.useState(1);
+    const [cryptoPercent, cryptoPercentChange] = React.useState(1);
 
     let fromDateUnix;
     let toDateUnix;
     (fromDate != null) ? fromDateUnix = fromDate.getTime() : fromDateUnix = "";
     (toDate != null) ? toDateUnix = toDate.getTime() : toDateUnix = "";
 
+    function handleFiatChange(e){
+      const re = /^[0-9\b]+$/;
+
+      // if value is not blank, then test the regex
+  
+      if (e.target.value === '' || re.test(e.target.value)){
+        startingFiatChange(e.target.value);
+      }
+    }
+    function handleCryptoChange(e){
+      const re = /^[0-9\b]+$/;
+
+      // if value is not blank, then test the regex
+  
+      if (e.target.value === '' || re.test(e.target.value)){
+        startingCryptoChange(e.target.value);
+      }
+    }
+    function handleFeeChange(e, newValue){
+      tradingFeeChange(newValue)
+    }
+    function handleCoinPercChange(e, newValue){
+      coinPercentChange(newValue)
+    }
+    function handleCryptoPercChange(e, newValue){
+      cryptoPercentChange(newValue)
+    }
+
     return (
       <div className="BacktestForm">
         <div>Current trading pair: {props.currentPair}</div>
-        <div>Date to simulate from:</div>
-        <DatePicker
-          onChange={fromDateChange}
-          value={fromDate}
-          required={true}
-          maxDate={toDate}
-        />
-        <div>Date to simulate to:</div>
-        <DatePicker
-          onChange={toDateChange}
-          value={toDate}
-          required={true}
-          maxDate={new Date()}
-          minDate={fromDate}
-        />
-        {fromDateUnix}
+        <div>Date to simulate from:&nbsp;
+          <DatePicker
+            onChange={fromDateChange}
+            value={fromDate}
+            required={true}
+            maxDate={toDate}
+          />
+        </div>
+        <div>Date to simulate to:&nbsp; 
+          <DatePicker
+            onChange={toDateChange}
+            value={toDate}
+            required={true}
+            maxDate={new Date()}
+            minDate={fromDate}
+          />
+        </div>
+        <div>Starting fiat balance</div>
+        <label>$<input type="text" value={startingFiat} onChange={handleFiatChange} /></label>
+        <div>Starting crypto balance</div>
+        <label>$<input type="text" value={startingCrypto} onChange={handleCryptoChange} /></label>
+        <div>Trading fee %</div>
+        <Slider value={tradingFee} onChange={handleFeeChange} valueLabelDisplay="on" min={0} max={2} step={0.01}/>
+        <div>Coin %</div>
+        <Slider value={coinPercent} onChange={handleCoinPercChange} valueLabelDisplay="on" min={0} max={1} step={0.01}/>
+        <div>Crypto %</div>
+        <Slider value={cryptoPercent} onChange={handleCryptoPercChange} valueLabelDisplay="on" min={0} max={1} step={0.01}/>
       </div>
     )
   }
@@ -86,3 +132,4 @@ export default function CoverPage(props) {
       </div>
     )
   }
+
